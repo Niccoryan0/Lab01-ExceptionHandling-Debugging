43ffd43931b0086a70cb6ac4b8d2029f59009dec
Nicco Ryan <niccoryan0@gmail.com>
2020-07-06 12:51:24 -0700

    Initial scaffolding

---
+++ Lab01a-ExceptionHandling/Lab01a-ExceptionHandling/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab01a_ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
