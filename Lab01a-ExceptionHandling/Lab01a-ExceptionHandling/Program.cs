ff320cc5583d9009a631441f290b58b37f032cd5
Nicco Ryan <niccoryan0@gmail.com>
2020-07-06 13:41:11 -0700

    Finished Populate and started GetSum method

--- Lab01a-ExceptionHandling/Lab01a-ExceptionHandling/Program.cs
+++ Lab01a-ExceptionHandling/Lab01a-ExceptionHandling/Program.cs
@@ -23,22 +23,53 @@ namespace Lab01a_ExceptionHandling
 
         static void StartSequence()
         {
-            Console.WriteLine("Please enter any number greater than 0");
-            string userChoiceStr = Console.ReadLine();
-            int userChoice = Convert.ToInt32(userChoiceStr);
-            int[] newArr = new int[userChoice];
-            int[] popArr = Populate(newArr);
-            // int sumResult = GetSum(popArr);
-            // int prodResult = GetProduct(popArr, prodInt);
-            // int quotResult = GetQuotient(prodResult);
-            Console.WriteLine($"The array you made is size {userChoice}");
-            Console.WriteLine($"The numbers in it are: {string.Join(", ", newArr)}");
-            // Console.WriteLine($"It's sum is {sumResult}");
-            // Console.WriteLine($"{sumResult} times {prodInt} = {prodResult}");
-            // Console.WriteLine($"{prodResult} divided by {quotInt} = {quotResult}");
+            try
+            {
+                Console.WriteLine("Please enter any number greater than 0");
+                string userChoiceStr = Console.ReadLine();
+                int userChoice = Convert.ToInt32(userChoiceStr);
+                int[] newArr = new int[userChoice];
+                int[] popArr = Populate(newArr);
+                // int sumResult = GetSum(popArr);
+                // int prodResult = GetProduct(popArr, prodInt);
+                // int quotResult = GetQuotient(prodResult);
+                Console.WriteLine($"The array you made is size {userChoice}");
+                Console.WriteLine($"The numbers in it are: {string.Join(", ", newArr)}");
+                // Console.WriteLine($"It's sum is {sumResult}");
+                // Console.WriteLine($"{sumResult} times {prodInt} = {prodResult}");
+                // Console.WriteLine($"{prodResult} divided by {quotInt} = {quotResult}");
+            }
+            catch (FormatException f)
+            {
+                Console.WriteLine(f.Message);
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        static int[] Populate(int[] myArr)
+        {
+            for (int i = 0; i < myArr.Length; i++)
+            {
+                Console.WriteLine($"Please enter a number: ({i} of {myArr.Length})");
+                // Get a number from the user for each position in the array, convert them to ints, and populate the array
+                string userChoice = Console.ReadLine();
+                int numChoice = Convert.ToInt32(userChoice);
+                myArr[i] = numChoice;
+            }
+            return myArr;
         }
 
-        static 
+        static int GetSum(int[] myArr)
+        {
+            int sum = 0;
+            foreach (int num in myArr)
+            {
+                sum += num;
+            }
+            return sum;
+        }
 
     }
 }
