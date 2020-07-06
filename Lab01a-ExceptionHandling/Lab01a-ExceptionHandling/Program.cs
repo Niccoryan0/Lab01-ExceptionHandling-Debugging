b757c701d42e7a1a1d65d4e4334ac5d49d0c2479
Nicco Ryan <niccoryan0@gmail.com>
2020-07-06 13:06:59 -0700

    Small test

--- Lab01a-ExceptionHandling/Lab01a-ExceptionHandling/Program.cs
+++ Lab01a-ExceptionHandling/Lab01a-ExceptionHandling/Program.cs
@@ -6,7 +6,7 @@ namespace Lab01a_ExceptionHandling
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hi!");
         }
     }
 }
